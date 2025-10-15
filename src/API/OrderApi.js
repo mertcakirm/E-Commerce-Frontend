dff76e49693bb4db08446ce34c6d6894f64b6299
mertcakirm <mertcakirm3377@gmail.com>
2025-10-15 14:17:31 +0300

    .

--- src/API/OrderApi.js
+++ src/API/OrderApi.js
@@ -1,5 +1,8 @@
 import api from "./api.js";
 
+export const GetMyOrdersRequest = async ()=>{
+    return await api.get("/Orders");
+}
 export const CreateOrderRequest = async (data) => {
-    return await api.post(`Orders`, data)
+    return await api.post(`Orders`, data);
 }
