a5cb6e644690580e102a7d13ff7c8b2af74d0ec2
mertcakirm <mertcakirm3377@gmail.com>
2025-10-20 20:05:24 +0300

    .

--- src/components/other/ProductQuestions.jsx
+++ src/components/other/ProductQuestions.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { GetProductQuestionsRequest, PostProductQuestionRequest } from "../../API/ProductApi.js";
+import {useEffect, useState} from 'react';
+import {GetProductQuestionsRequest, PostProductQuestionRequest} from "../../API/ProductApi.js";
 import {getCookie} from "../cookie/cookie.js";
 import {HiOutlineChevronLeft, HiOutlineChevronRight} from "react-icons/hi";
+import {toast} from "react-toastify";
 
-const ProductQuestions = ({ productId }) => {
+const ProductQuestions = ({productId}) => {
     const [questions, setQuestions] = useState([]);
     const [pageNum, setPageNum] = useState(1);
     const [newQuestion, setNewQuestion] = useState("");
@@ -15,21 +16,27 @@ const ProductQuestions = ({ productId }) => {
         const response = await GetProductQuestionsRequest(productId, pageNum);
         if (response && response.data.items) {
             setQuestions(response.data.items || []);
+            setLastPage(response.data.totalPages || 0);
         }
     };
 
     const SendQuestion = async () => {
-        if (!newQuestion.trim()) return;
+        if (!newQuestion.trim()) return toast.error("Lütfen sorunuzu yazınız!!");
         const body = {
             productId: productId,
             questionText: newQuestion
         };
 
-        const res = await PostProductQuestionRequest(body);
-        if (res.status === 200 || res.status === 201) {
+        try {
+            await PostProductQuestionRequest(body);
             setNewQuestion("");
+            toast.success("Soru başarıyla gönderildi!")
             GetQuestions();
+        } catch (error) {
+            console.log(error);
+            toast.error("Soru gönderilirken bir hata oluştu!")
         }
+
     };
 
     useEffect(() => {
@@ -44,7 +51,7 @@ const ProductQuestions = ({ productId }) => {
                     <textarea
                         className="mb-2"
                         rows={3}
-                        style={{height:'150px',resize:'none'}}
+                        style={{height: '150px', resize: 'none'}}
                         placeholder="Bu ürün hakkında soru sor..."
                         value={newQuestion}
                         onChange={(e) => setNewQuestion(e.target.value)}
@@ -61,16 +68,21 @@ const ProductQuestions = ({ productId }) => {
                 )}
 
                 {questions.map((q) => (
-                    <div key={q.id} className="card p-3 mb-2 shadow-sm">
+                    <div key={q.id} className="card p-3 mb-2 shadow-sm" style={{minWidth: '250px', maxWidth: '100%'}}>
                         <div className="d-flex justify-content-between">
                             <span className="fw-bold">{q.userEmail}</span>
-                            <span className="text-muted" style={{ fontSize: "12px" }}>
+                            <span className="text-muted" style={{fontSize: "12px"}}>
                                 {new Date(q.created).toLocaleDateString("tr-TR")}
                             </span>
                         </div>
-                        <p className="mt-2 mb-1">{q.question}</p>
+                        <p className="mt-2 mb-1"
+                           style={{maxWidth: '100%', textWrap: 'wrap', wordBreak: 'break-all'}}>{q.question}</p>
                         {q.answer ? (
-                            <p className="text-success small">Cevap: {q.answer}</p>
+                            <p className="text-success small" style={{
+                                maxWidth: '100%',
+                                textWrap: 'wrap',
+                                wordBreak: 'break-all'
+                            }}>Cevap: {q.answer}</p>
                         ) : (
                             <p className="text-danger small">Henüz cevaplanmadı</p>
                         )}
@@ -80,14 +92,16 @@ const ProductQuestions = ({ productId }) => {
             </div>
             <div className="d-flex gap-2 justify-content-center">
                 {pageNum > 1 &&
-                    <button className="btn pag-btn d-flex justify-content-center align-items-center" onClick={() => setPageNum(pageNum - 1)}>
-                        <HiOutlineChevronLeft size={20} />
+                    <button className="btn pag-btn d-flex justify-content-center align-items-center"
+                            onClick={() => setPageNum(pageNum - 1)}>
+                        <HiOutlineChevronLeft size={20}/>
                     </button>
                 }
                 <button className="btn pag-btn">{pageNum}</button>
                 {pageNum < lastPage &&
-                    <button className="btn pag-btn d-flex justify-content-center align-items-center" onClick={() => setPageNum(pageNum + 1)}>
-                        <HiOutlineChevronRight size={20} />
+                    <button className="btn pag-btn d-flex justify-content-center align-items-center"
+                            onClick={() => setPageNum(pageNum + 1)}>
+                        <HiOutlineChevronRight size={20}/>
                     </button>
                 }
             </div>
